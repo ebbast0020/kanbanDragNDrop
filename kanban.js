428509fa89c02718ad9fa5cde2dc2c72c3327f9b
Ebba Asteräng <ebbast0020@skynet.halmstad.se>
2018-09-06 14:42:42 +0200

    dragNdrop start

--- kanban.js
+++ kanban.js
@@ -1,119 +1,68 @@
+var kanban = document.getElementsByClassName("kanban");
+kanban = Array.from(kanban);
 
-window.addEventListener("keydown", checkKey);
-
-
-function checkKey(ev){     if(ev.keyCode === 13)    
-    {       
-      saveNewTask();    }     }    
-      const mySaveButton = document.getElementById("saveNewTask");
-
-// lägg till klick på save-knappen
-mySaveButton.addEventListener("click",saveNewTask);  
-function saveNewTask(){ 
-    
-    //Hämta textrutans innehåll
-    const text = document.getElementById("newTask"); 
-    
-    if(text.value.trim().length > 0)
-    {
-
-    console.log(text.value); 
-    
-    //Skapa en ny div
-    var divTask = document.createElement("div"); 
-    divTask.className = "createdTask"
-
-
-    //Lägg till lyssnare till vårt nya element 
-    divTask.addEventListener("click",moveToNext);
-    
-  
-        
-
-        const firstKanban = document.getElementsByClassName("kanban")[0];
-        console.log(firstKanban);
-
-
-        console.log("new Element is clicked"); 
-   
-
-
-    //Skapa en text-node som du kan lägga in i din nya div
-    var theText = document.createTextNode(text.value); 
-   
-    //Lägg till text-noden till din nyskapde div
-    divTask.appendChild(theText); 
-
-    //Lägg till tillbaka-knapp
-    var backButton = document.createElement("button");
-    backButton.className = "backButton";
-    backButton.innerHTML = "&larr;";
-    backButton.addEventListener("click", moveBack);
-    divTask.appendChild(backButton);
-
-
-    
-
-
-
-
-
-
-    //Lägg till nya diven i föräldern till knappen du tryckte på
-    firstKanban.appendChild(divTask); 
-    
- 
-
-
-    } //end if
-
-        //Tömmer textruta
-        text.value= "";   
-        
-} //end function
-
-
-function moveToNext(){
-
-//vi måste hitta vilket index vår nuvarande förälder har, detta för att kunna byta ut föräldern mot en bättre.
-
-const kanban = Array.from(document.getElementsByClassName("kanban"));
-
-//leta efter index för föräldern jag kom ifrån
-let currentParentIndex = kanban.indexOf(this.parentElement);
-
-
-console.log(currentParentIndex);
-
-if(currentParentIndex<kanban.length-1)
+for(let i in kanban)
 {
-currentParentIndex = currentParentIndex +1;
-kanban[currentParentIndex].appendChild(this);
-}
-
-
-}
-
-function moveBack(ev){
-
-event.stopPropagation();
-   const clickedParent = this.parentElement;
-   const kanban = Array.from(document.getElementsByClassName("kanban"))
-   let currentParentIndex = kanban.indexOf(clickedParent.parentElement);
-
-   if(currentParentIndex > 0)
-   {
-currentParentIndex -= 1;
-kanban[currentParentIndex].appendChild(clickedParent);
-
-   }
+    console.log(kanban[i]);
+    kanban[i].addEventListener("dragover", dragOver);
+    kanban[i].addEventListener("drop", drop);
 
 
 
 }
 
-/*function moveToDone(){
 
-    const thirdKanban = document.getElementsByClassName("kanban")[3];
-    thirdKanban.appendChild(this);
-}*/
+
+
+//  lägg till klick på knapp
+const newTaskButton = document.getElementById("saveNewTask");
+newTaskButton.addEventListener("click", saveNewTask);
+
+function saveNewTask(){
+
+    const newTaskText = document.getElementById("newTask").value;
+    const myTemplate = document.getElementById("todoTemplate");
+
+    let newTemplate = myTemplate.cloneNode(true);
+    newTemplate.children[0].innerHTML= newTaskText;
+
+    newTemplate.className = "todoTemplate";
+
+    let id = "_"+ Date.now();
+    newTemplate.id=id;
+
+    let kanban = document.getElementsByClassName("kanban")[0];
+    kanban.appendChild(newTemplate);
+
+    newTemplate.addEventListener("dragstart",dragStart);
+    newTemplate.addEventListener("dragend",dragEnd);
+
+
+    
+}
+
+
+var draggedEl = "";
+
+function dragStart(){
+draggedEl = this;
+}
+
+function dragEnd(){
+
+    
+}
+
+function dragOver(ev){
+    ev.preventDefault();
+
+
+}
+
+function drop(){
+
+    this.appendChild(draggedEl);
+
+
+
+}
---
+++ kanban2.js
@@ -0,0 +1,1 @@
+//  lägg till klick på knapp
